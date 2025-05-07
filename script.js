7cbe052f1cdee63fe33241b85f254bcc252d119c
ah3863 <ah3863@nyu.edu>
2025-05-07 11:39:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const initialViewState = {
+  latitude: 40.7,     // adjust slightly if needed
+  longitude: -73.94,  // adjust slightly if needed
+  zoom: .1,            // lower value = zoomed out
+  bearing: 0,
+  pitch: 27
+};
+
+let cdVisible = false;
+let floodVisible = false;
+let redlineVisible = true;
+
+document.getElementById('toggle-flood').addEventListener('click', () => {
+    floodLayerVisible = !floodLayerVisible;
+    map.setLayoutProperty('flood-risk-layer', 'visibility', floodLayerVisible ? 'visible' : 'none');
+    map.setLayoutProperty('flood-risk-outline', 'visibility', floodLayerVisible ? 'visible' : 'none');
+  });
+  
+  document.getElementById('toggle-cdbg').addEventListener('click', () => {
+    cdbgLayerVisible = !cdbgLayerVisible;
+    map.setLayoutProperty('community-layer', 'visibility', cdbgLayerVisible ? 'visible' : 'none');
+  });
+
+  document.getElementById('toggle-redline').addEventListener('click', () => {
+    redlineLayerVisible = !redlineLayerVisible;
+    map.setLayoutProperty('redline-layer', 'visibility', redlineLayerVisible ? 'visible' : 'none');
+  });
+  
+  
